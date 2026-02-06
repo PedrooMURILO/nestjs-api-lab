2e280bdcd34c2282343d1370af4ffdb6ac9e1e20
Pedro Murilo <pedromarmindo@gmail.com>
2026-02-06 12:09:16 -0300

    Creating My First TypeORM Entity in NestJS (My First Table in the PostgreSQL Database)

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
 import { MessagesService } from './messages.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MessagesEntity } from './messages-entity/messages-entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MessagesEntity])],
   controllers: [MessagesController],
   providers: [MessagesService],
 })
