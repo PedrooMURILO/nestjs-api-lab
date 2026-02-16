e287566192aac8b594abc7b6894abdbb3c04861d
Pedro Murilo <pedromarmindo@gmail.com>
2026-02-16 18:04:31 -0300

    Person CRUD: Creating the Create Operation with a Unique Email in the Table

--- src/persons/dto/create-person.dto.ts
+++ src/persons/dto/create-person.dto.ts
@@ -1,1 +1,22 @@
-export class CreatePersonDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreatePersonDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  name: string;
+}
--- src/persons/persons.service.ts
+++ src/persons/persons.service.ts
@@ -1,11 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Person } from './entities/person.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PersonsService {
-  create(createPersonDto: CreatePersonDto) {
-    return 'This action adds a new person';
+  constructor(
+    @InjectRepository(Person)
+    private readonly personRepository: Repository<Person>,
+  ) {}
+
+  async create(createPersonDto: CreatePersonDto) {
+    try {
+      const personData = {
+        name: createPersonDto.name,
+        passwordHash: createPersonDto.password,
+        email: createPersonDto.email,
+      };
+      return this.personRepository.save(
+        this.personRepository.create(personData),
+      );
+    } catch (error) {
+      if (error.code === '23505')
+        throw new ConflictException('E-mail already registered');
+      throw error;
+    }
   }
 
   findAll() {
